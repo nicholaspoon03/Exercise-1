6329c53324e675e9dea253473cc7dd3cde411e5f
nicholaspoon03 <47435386+nicholaspoon03@users.noreply.github.com>
2020-03-19 16:34:32 -0400

    Add files via upload

---
+++ mailing_address.py
@@ -0,0 +1,9 @@
+name = 'Nicholas Poon'
+street = '8101 Leslie Street'
+city = 'Thornhill'
+postal_code = 'L3T 7P4'
+province = 'ON'
+
+print(name)
+print(street)
+print(city + ', ' + province + ' ' + postal_code)
